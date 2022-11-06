b5262b122f7b4bac8e30f2c87d03f8e8fa403ffe
Marvin-kamwenji <90758968+Marvin-kamwenji@users.noreply.github.com>
2022-11-06 09:31:54 +0300

    working on checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import "./Checkout.css";
+import CheckoutProduct from './CheckoutProduct';
+import Subtotal from './Subtotal';
 
 function Checkout() {
   return (
-    <div>
-        <h1>Hello</h1>
+    <div className="checkout">
+     <div className="checkout__left">
+         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0qeE6-STIkb_8QldpoAg3KhBQAmveMNH3-6twJQDPOt5A3hd6ROzo03xEND-zPnCA5Cc&usqp=CAU" alt="" className="checkout__ad" />
+         <div>
+            <h2 className="checkout__title">
+                Your Shopping Basket
+            </h2>
+            <CheckoutProduct />
+          </div>
+     </div>
+     <div className="checkout__right">
+      <Subtotal />
+     </div>
     </div>
   )
 }
